b11885c20fe4739c0e4c3e426ed0197eb4510f8f
Justin <19782272+kyotodesertfox@users.noreply.github.com>
2026-04-26 00:15:31 -0400

    Added Signature in Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,6 +7,9 @@ const Footer = () => {
 
         {/* Main Footer Content */}
         <p className="text-zinc-500 text-xs uppercase tracking-[0.2em] mb-2">
+        CRAFTED FOR ARCWRIGHT BY <span className="text-zinc-200"><a href="mailto:zenko18@gmail.com?subject=ArcWright%20Website">Justin</a></span>
+        </p>
+        <p className="text-zinc-500 text-xs uppercase tracking-[0.2em] mb-2">
         © {new Date().getFullYear()} ARCWRIGHT WELDING | JACKSONVILLE, FL
         </p>
         <p className="text-zinc-700 text-[10px] uppercase tracking-widest font-bold">
@@ -21,8 +24,8 @@ const Footer = () => {
         className="w-24 h-24 md:w-48 md:h-48 brightness-150 pointer-events-none"
         />
         </div>
-
         </div>
+
         </footer>
     );
 };
