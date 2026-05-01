0fe97a2b6ac456b9035fb7da119fe3091a482b96
Justin <19782272+kyotodesertfox@users.noreply.github.com>
2026-04-30 21:47:53 -0400

    Updated Admin Portal

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -3,18 +3,22 @@ const axios = require('axios');
 exports.handler = async (event) => {
     const { code } = event.queryStringParameters;
 
-    // These come from your Netlify Env Variables (Not the frontend!)
-    const client_id = process.env.VITE_GITHUB_CLIENT_ID;
-    const client_secret = process.env.GITHUB_CLIENT_SECRET;
-
     try {
+
         const res = await axios.post('https://github.com/login/oauth/access_token', {
-            client_id,
-            client_secret,
+            client_id: process.env.VITE_GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
             code
         }, {
             headers: { Accept: 'application/json' }
         });
+
+        if (res.data.error) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ error: res.data.error_description })
+            };
+        }
 
         return {
             statusCode: 200,
@@ -23,7 +27,7 @@ exports.handler = async (event) => {
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: error.message })
+            body: JSON.stringify({ error: "Authentication handshake failed" })
         };
     }
 };
